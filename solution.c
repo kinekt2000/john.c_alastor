5225016d66197bd25db7cc02a5c1579893a4df5f
Constantine <kinekt2000@gmail.com>
2019-03-05 18:50:58 +0300

    lab3 changed

--- solution.c
+++ solution.c
@@ -12,21 +12,13 @@ int main(){
 	int count = 0;
 	char path[] = "./";
 	content(path, &cont, &count);
-	printf("%d\n", count);
-	for(int i = 0; i < count; i++)
-		printf("%s", cont[i]);
-
 	qsort(cont, count, sizeof(char*), cmp);
-	printf("\nSorted\n");
-
-	for(int i = 0; i < count; i++)
-                printf("%s", cont[i]);
 
 	FILE *result = fopen("./result.txt", "wb");
 	for(int i = 0; i < count; i++)
-		fprintf(result, "%s", cont[i]);
+		if (cont[i][strlen(cont[i])-2] != '\n')
+			fprintf(result, "%s", cont[i]);
 	fclose(result);
-
 	if(count){
 		for(int i = 0; i < count; i++) free(cont[i]);
 		free(cont);
@@ -54,7 +46,7 @@ void content(char *path, char ***cont, int *count){
 		struct dirent *de = readdir(dir);
 		while(de){
 			if (de->d_type == 4 && strcmp(de->d_name, "..") && strcmp(de->d_name, ".")){ /* is dir (not . & ..)*/
-				printf("open this directory: %s\n", de->d_name);
+				//printf("open this directory: %s\n", de->d_name);
 				char nested_path[100] = "";
 				strcpy(nested_path, path);
 				strcat(nested_path, "/");
@@ -62,12 +54,6 @@ void content(char *path, char ***cont, int *count){
 				content(nested_path, cont, count);
 			}
 			else if (de->d_type == 8 && strcmp(de->d_name+strlen(de->d_name)-4, ".txt")==0){
-
-				/*
-					Программа создает и заполняет массив названиями файлов, которые кончаются на .txt
-					Нужно с помощью fopen открывать файл path+d_name и сохранить первую строку оттуда в
-					char info[100] и уже  заполнять массив не именами, а содержимым
-				*/
 
 				char* sent = (char*)calloc(102, sizeof(char));
 				char txtpath[100] = "";
@@ -84,18 +70,6 @@ void content(char *path, char ***cont, int *count){
                                         else exit(1);
                                 }
 				(*cont)[*count] = sent;
-
-				/*char* name = (char*)calloc(strlen(de->d_name)+2, sizeof(char));
-				strcpy(name, de->d_name);
-				strcat(name, "\n");
-				if (!(*count)) *cont = (char**)malloc(sizeof(char*));
-				else{
-					char **buffer = (char**)realloc(*cont, ((*count)+1)*sizeof(char*));
-					if (buffer) *cont = buffer;
-					else exit(1);
-				}
-				(*cont)[*count] = name;*/
-
 				(*count)++;
 			}
 			de = readdir(dir);
@@ -103,4 +77,3 @@ void content(char *path, char ***cont, int *count){
 	}
 	closedir(dir);
 }
-
